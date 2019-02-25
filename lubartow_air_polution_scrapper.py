f19d6ea2ff3a42103d39772c71a2cd3f073fcbb3
nieweglp <nieweglp@gmail.com>
2019-02-25 22:14:31 +0100

    new file:   Maps.ipynb s clear new file:   lubartow_air_polution_scrapper.py

---
+++ lubartow_air_polution_scrapper.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 17:05:26 2019
+
+@author: Paul
+"""
+# web scrapping lubartow24.pl info about air polution
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import datetime
+import pandas as pd
+
+
+url = 'https://lubartow24.pl/'
+
+soup = BeautifulSoup(requests.get(url).text, 'lxml')
+
+soup.find_all('div',class_='panel')[-6:]
+
+now = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+l = []
+for element in soup.find_all('div',class_='panel')[-6:]:
+	l.append(now + '\n' + 
+	    element.get_text().strip().replace(' · ','\n'))
+
+
+str_list = ['Indeks Jakości Powietrza:  ','PM2.5: ','PM10: ']
+str_place = ['INTERDUO.pl','Kancelaria']
+
+ln = []
+for i in l:
+	ni = re.sub('|'.join(str_list),'',re.sub('|'.join(str_place),'Lubartów',i))
+	ln.append(ni.splitlines())
+	
+
+df = pd.DataFrame(ln, columns=['timestamp','loc','status','PM2_5','PM10'])
+
+df
+now = now.replace(':','')
+out_name = ('C:/Users/Paul/Desktop/python/datasets/Lubartow_air' + now + '.csv')
+df.to_csv(out_name,index=False)
+
+
+
+
