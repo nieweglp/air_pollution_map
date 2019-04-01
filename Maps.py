a57df5dca271b227be6bb48372379535eccc8f1c
nieweglp <nieweglp@gmail.com>
2019-04-01 22:18:38 +0200

    last change

---
+++ Maps.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import folium
+import pandas as pd
+from geopy.geocoders import Nominatim
+from folium.plugins import MarkerCluster
+import webbrowser
+
+df = pd.read_csv(r'C:\Users\Paul\Desktop\python\datasets\Lubartow_air.csv')
+
+geolocator = Nominatim(user_agent="specify_your_app_name_here")
+addr = []
+for i in range(len(df)):
+    addr.append(geolocator.geocode(df['loc'][i]))
+
+addr_loc = []
+for i in addr:
+    addr_loc.append(i[-1])
+
+df_loc = pd.DataFrame(addr_loc,columns=['latitude','longitude'])
+
+df = pd.concat([df,df_loc],axis=1)
+
+df_col = pd.DataFrame({'Status': ["Bardzo dobry", "Dobry", "Umiarkowany", "Dostateczny", "Zły", "Bardzo zły"],
+              'Color': ['blue','green','yellow','orange','red','black']})
+
+m = folium.Map(addr_loc[0],zoom_start=13)
+
+for i in range(len(addr_loc)):
+    info = 'Status: ' + df.iloc[i]['status'] +'<br>PM 2.5: '+ str(df.iloc[i]['PM2_5']) +     '<br>PM 10: '+ str(df.iloc[i]['PM10'])
+   
+    marker_cluster = MarkerCluster().add_to(m)
+    
+    folium.CircleMarker(
+        location=addr_loc[i],
+        popup=info,
+        radius=20,
+        color=str(df_col.loc[df_col['Status'] == df.iloc[i]['status'],['Color']]).rsplit()[-1],
+        fill=True
+    ).add_to(marker_cluster)
+
+filepath = 'C:/Users/Paul/Desktop/maps.html'
+
+m.save(filepath)
+webbrowser.open('file://' + filepath)
--- lubartow_air_polution_scrapper.py
+++ lubartow_air_polution_scrapper.py
@@ -12,37 +12,41 @@ import re
 import datetime
 import pandas as pd
 
+def air_scrapper():
+	url = 'https://lubartow24.pl/'
+	soup = BeautifulSoup(requests.get(url).text, 'lxml')
 
-url = 'https://lubartow24.pl/'
-
-soup = BeautifulSoup(requests.get(url).text, 'lxml')
-
-soup.find_all('div',class_='panel')[-6:]
-
-now = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
-l = []
-for element in soup.find_all('div',class_='panel')[-6:]:
-	l.append(now + '\n' + 
-	    element.get_text().strip().replace(' · ','\n'))
-
-
-str_list = ['Indeks Jakości Powietrza:  ','PM2.5: ','PM10: ']
-str_place = ['INTERDUO.pl','Kancelaria']
-
-ln = []
-for i in l:
-	ni = re.sub('|'.join(str_list),'',re.sub('|'.join(str_place),'Lubartów',i))
-	ln.append(ni.splitlines())
+	soup.find_all('div',class_='panel')[-6:]
 	
+	now = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+	
+	l = []
+	for element in soup.find_all('div',class_='panel')[-6:]:
+		l.append(now + '\n' + 
+		    element.get_text().strip().replace(' · ','\n'))
+	
+	str_list = ['Indeks Jakości Powietrza:  ','PM2.5: ','PM10: ']
+	str_place = ['INTERDUO.pl','Kancelaria']
+	
+	ln = []
+	for i in l:
+		ni = re.sub('|'.join(str_list),'',re.sub('|'.join(str_place),'Lubartów',i))
+		ln.append(ni.splitlines())
+		
+	df = pd.DataFrame(ln, columns=['timestamp','loc','status','PM2_5','PM10'])
+	
+	now = now.replace(':','')
+	out_name = ('C:/Users/Paul/Desktop/python/datasets/Lubartow_air' + now + '.csv')
+	
+	return df.to_csv(out_name,index=False)
+	
+air_scrapper()
 
-df = pd.DataFrame(ln, columns=['timestamp','loc','status','PM2_5','PM10'])
-
-df
-now = now.replace(':','')
-out_name = ('C:/Users/Paul/Desktop/python/datasets/Lubartow_air' + now + '.csv')
-df.to_csv(out_name,index=False)
-
+try:
+	air_scrapper()
+	print("You scrapped air polution data succesfully!")
+except:
+	print("Your script has falied :( !")
 
 
 
